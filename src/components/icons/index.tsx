84056bd7c3641da59317051f52fef186d9911dd4
Julio Cesar Lima Reis <juliolimareis@gmail.com>
2023-01-13 20:47:40 -0300

    Ajuste no tamanho padrão dos icones

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -6,6 +6,7 @@ type SVGProps = React.SVGProps<SVGSVGElement>
 function SVG(props: SVGProps): JSX.Element {
   return (
     <svg
+      className="w-6"
       {...props}
       xmlns="http://www.w3.org/2000/svg"
       version="1.1"
@@ -47,9 +48,20 @@ export function ArrowBottom(props: SVGProps): JSX.Element {
   return (
     <SVG
       {...props}
-      viewBox="0 0 96 96"
+      viewBox="0 0 20 20"
     >
-      <path d="M81.8457,25.3876a6.0239,6.0239,0,0,0-8.45.7676L48,56.6257l-25.396-30.47a5.999,5.999,0,1,0-9.2114,7.6879L43.3943,69.8452a5.9969,5.9969,0,0,0,9.2114,0L82.6074,33.8431A6.0076,6.0076,0,0,0,81.8457,25.3876Z"/>
+      <path d="M 2 5.2929688 L 1.2929688 6 L 10 14.707031 L 18.707031 6 L 18 5.2929688 L 10 13.292969 L 2 5.2929688 z "/>
+    </SVG>
+  );
+}
+
+export function ArrowRight(props: SVGProps): JSX.Element {
+  return (
+    <SVG
+      {...props}
+      viewBox="0 0 24 24"
+    >
+      <polygon points="6.8,23.7 5.4,22.3 15.7,12 5.4,1.7 6.8,0.3 18.5,12"/>
     </SVG>
   );
 }
