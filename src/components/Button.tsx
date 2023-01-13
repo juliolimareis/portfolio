5c36428972a85fcc410b8f45bc20744b6fc32547
Julio Cesar Lima Reis <juliolimareis@gmail.com>
2023-01-13 20:46:10 -0300

    Ajuste nas cores do botão

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,31 +4,40 @@ type ButtonProps = React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButton
 
 export default function Button(props: ButtonProps): JSX.Element {
   return (
-    <button
-      {...props}
-      className={`
-        bg-primary
-        text-bt-text
-        hover:bg-hover-bt-bg
-        
-        -dark:bg-dark-bt-bg
-        dark:text-dark-bt-text
-        dark:hover:bg-dark-hover-bt-bg
-  
-        font-bold
-        py-2
-        px-4
-        rounded
-        inline-flex
-        items-center
-        transition
-        duration-300
-        ${props.className}
-        dark:bg-neon
-        dark:animate-spin-slow
-      `}
-    >
-      {props.children}
+    <button className="
+      p-1
+      dark:bg-neon
+      rounded
+      inline-flex
+      items-center
+      dark:animate-spin-slow
+    ">
+      <button
+        {...props}
+        className={`
+          bg-primary
+          text-bt-text
+          hover:bg-hover-bt-bg
+          
+          dark:bg-dark-bt-bg
+          dark:text-dark-bt-text
+          dark:hover:bg-dark-hover-bt-bg
+    
+          font-bold
+          rounded
+          inline-flex
+          items-center
+          transition
+          duration-300
+          py-3
+          px-5
+          ${props.className}
+          -bg-gradient-to-r from-indigo-500 via-blue-500 to-pink-500
+          -night
+        `}
+      >
+        {props.children}
+      </button>
     </button>
   );
 }
