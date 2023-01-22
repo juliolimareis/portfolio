5659da9f0056689e4c9e48117a80a65859891410
Júlio César Lima Reis <juliolimareis@gmail.com>
2023-01-22 16:55:56 -0300

    Add icone para navbar

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -109,3 +109,14 @@ export function Github(props: SVGProps): JSX.Element {
     </SVG>
   );
 }
+
+export function NavbarHamburger(props: SVGProps): JSX.Element {
+  return (
+    <SVG
+      {...props}
+      viewBox="0 0 20 20"
+    >
+      <path d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"/>
+    </SVG>
+  );
+}
